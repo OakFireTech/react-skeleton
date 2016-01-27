a7a6ff3d3be2670d7cbbb68d40568939bea955b3
Michael <michael@oakfiretech.com>
2016-01-26 22:09:19 -0500

    Cleaning up the routing hash

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,12 +3,17 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 
+var CreateHistory = require('history/lib/createHashHistory');
+var History = new CreateHistory({
+    queryKey: false
+});
+
 var Base = require('./components/Base.jsx');
 var About = require('./components/About.jsx');
 var Contact = require('./components/Contact.jsx');
 
 var Routes = (
-    <Router>
+    <Router history={History}>
         <Route path="/" component={Base} >
             <Route path="/about" component={About} />
             <Route path="/contact" component={Contact} />
